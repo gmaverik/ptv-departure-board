afb3000c8d09ac6d2b65e0b2e3395ee5e3464cbb
Maverik Grassby <119048402+gmaverik@users.noreply.github.com>
2023-02-02 10:02:46 +1100

    Added some stopping lists to stationNames.js

--- stationNames.js
+++ stationNames.js
@@ -22,16 +22,54 @@ var stationNames = {
     1071 : 'Flinders Street',
 }
 
-var test = "I sure hope this works";
-
 // Train Line Stopping list
 
 var alameinLine = {
-
+    1002 : 'Alamein',
+    1010 : 'Ashburton',
+    1031 : 'Burwood',
+    1087 : 'Hartwell',
+    1213 : 'Willison',
+    1166 : 'Riversdale',
+    1032 : 'Camberwell',
+    1012 : 'Auburn',
+    1080 : 'Glenferrie',
+    1090 : 'Hawthorn',
+    1030 : 'Burnley',
+    1059 : 'East Richmond',
+    1162 : 'Richmond',
+    1071 : 'Flinders Street',
 }
 
 var belgraveLine = {
-
+    1018 : 'Belgrave',
+    1191 : 'Tecoma',
+    1200 : 'Upwey',
+    1199 : 'Upper Ferntree Gully',
+    1067 : 'Ferntree Gully',
+    1025 : 'Boronia',
+    1016 : 'Bayswater',
+    1092 : 'Heathmont',
+    1163 : 'Ringwood',
+    1091 : 'Heatherdale',
+    1128 : 'Mitcham',
+    1148 : 'Nunawading',
+    1023 : 'Blackburn',
+    1111 : 'Laburnum',
+    1026 : 'Box Hill',
+    1129 : 'Mont Albert',
+    1189 : 'Surrey Hills',
+    1037 : 'Chatham',
+    1033 : 'Canterbury',
+    1057 : 'East Camberwell',
+    1032 : 'Camberwell',
+    1012 : 'Auburn',
+    1080 : 'Glenferrie',
+    1090 : 'Hawthorn',
+    1030 : 'Burnley',
+    1059 : 'East Richmond',
+    1162 : 'Richmond',
+    1071 : 'Flinders Street',
 }
 
 var craigieburnLine = {
@@ -47,7 +85,22 @@ var frankstonLine = {
 }
 
 var glenWaverlyLine = {
-
+    1078 : 'Glen Waverly',
+    1190 : 'Syndal',
+    1137 : 'Mount Waverly',
+    1105 : 'Jordanville',
+    1096 : 'Homesglen',
+    1058 : 'East Malvern',
+    1051 : 'Darling',
+    1077 : 'Glen Iris',
+    1075 : 'Gardiner',
+    1195 : 'Tooronga',
+    1110 : 'Kooyong',
+    1094 : 'Heyington',
+    1030 : 'Burnley',
+    1059 : 'East Richmond',
+    1162 : 'Richmond',
+    1071 : 'Flinders Street',
 }
 
 var hurstbridgeLine = {
@@ -55,7 +108,30 @@ var hurstbridgeLine = {
 }
 
 var lilydaleLine = {
-
+    1115 : 'Lilydale',
+    1133 : 'Mooroolbark',
+    1048 : 'Croydon',
+    1164 : 'Ringwood East',
+    1163 : 'Ringwood',
+    1091 : 'Heatherdale',
+    1128 : 'Mitcham',
+    1148 : 'Nunawading',
+    1023 : 'Blackburn',
+    1111 : 'Laburnum',
+    1026 : 'Box Hill',
+    1129 : 'Mont Albert',
+    1189 : 'Surrey Hills',
+    1037 : 'Chatham',
+    1033 : 'Canterbury',
+    1057 : 'East Camberwell',
+    1032 : 'Camberwell',
+    1012 : 'Auburn',
+    1080 : 'Glenferrie',
+    1090 : 'Hawthorn',
+    1030 : 'Burnley',
+    1059 : 'East Richmond',
+    1162 : 'Richmond',
+    1071 : 'Flinders Street',
 }
 
 var merndaLine = {
