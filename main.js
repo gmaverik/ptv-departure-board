287b6ca29deb77ecfc388710c81cb324cf5e69b0
Maverik Grassby <119048402+gmaverik@users.noreply.github.com>
2023-05-08 15:28:12 +1000

    Work on stopping list

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ var userPlatform = 1;
 var primaryRequest;
 var runRef0, runRef1, runRef2, runRef3, runRef4;
 var primaryRequest;
-
+var sorted = [];
+var j = 0;
+var currentRunRef = 000000;
 
 const urlQuery = window.location.search;
 const splitUrlQuery = urlQuery.split("/");
@@ -27,7 +29,6 @@ for (let j = 0; j < Object.keys(stations).length; j++) {
   }
   // console.log("The user station is located at array index: " + stationArrayIndex);
 }
-
 
 function date_toTime(date) // Converts "YYYY-MM-DDTHH:MM:SSZ" to "HH:MM" (plus 24 to 12h time) 
 {
@@ -54,10 +55,8 @@ function date_toUntil(date, schedTime)  // Gives an estimate for departure time
   return Math.ceil(difference / 60000);
 };
 
-
 // var hash = CryptoJS.HmacSHA1(comRequest, apiKey);
 // console.log("https://timetableapi.ptv.vic.gov.au" + comRequest + "&signature=" + hash.toString())
-
 
 setInterval(() => {
   
@@ -75,107 +74,97 @@ setInterval(() => {
   clockNow24 = `${hourNow24}:${minuteNow}:${secondNow}`;
   // console.log(`T${hourNow24}${minuteNow}${secondNow}`);
 
-  primaryRequest = "/v3/departures/route_type/0/stop/"+ userStation +"?platform_numbers=" + userPlatform + "&max_results=10000" + "&devid=" + devId;
+  primaryRequest = "/v3/departures/route_type/0/stop/"+ userStation +"?platform_numbers=" + userPlatform + "&max_results=1000&expand=all" + "&devid=" + devId;
 
 (async function() {
   const response = await fetch("http://timetableapi.ptv.vic.gov.au" + primaryRequest + "&signature=" + CryptoJS.HmacSHA1(primaryRequest, apiKey).toString());
   const json = await response.json();
   // console.info("http://timetableapi.ptv.vic.gov.au" + primaryRequest + "&signature=" + CryptoJS.HmacSHA1(primaryRequest, apiKey).toString())
   primaryResponse = json;
-  // console.log(primaryResponse);
-  runRef0 = primaryResponse.departures[0].run_ref;
-  runRef1 = primaryResponse.departures[1].run_ref;
-  runRef2 = primaryResponse.departures[2].run_ref;
-  runRef3 = primaryResponse.departures[3].run_ref;
-  runRef4 = primaryResponse.departures[4].run_ref;
+  userStationName = primaryResponse.stops[userStation].stop_name;
+  for (let i = 0; i < Object.keys(primaryResponse).length; i++) { // avoids any service with a direction id of 99 (City Loop), they break the system
+    if(primaryResponse.departures[i].direction_id != 99)
+    {
+      sorted[j] = i;
+      j++;
+    }
+  }
+// console.log(primaryResponse.stops[userStation].stop_name)
+  // for (let j = 0; j < Object.keys(stations).length; j++) { 
+  //   if(primaryResponse.stops[j].stop_id == userStation)
+  //   {
+  //     userStationName =  primaryResponse.stops[j].stop_name;   // console.log("The user station is: " + stopping_list[j].stationName);
+  //     break;
+  //   }
+  //   // console.log("The user station is located at array index: " + stationArrayIndex);
+  // }
 
-  sched0 = new Date(primaryResponse.departures[0].scheduled_departure_utc)
-  sched1 = new Date(primaryResponse.departures[1].scheduled_departure_utc)
-  sched2 = new Date(primaryResponse.departures[2].scheduled_departure_utc)
-  sched3 = new Date(primaryResponse.departures[3].scheduled_departure_utc)
-  sched4 = new Date(primaryResponse.departures[4].scheduled_departure_utc)
+  // console.log(Object.keys(primaryResponse).length);
+  runRef0 = primaryResponse.departures[sorted[0]].run_ref;
+  runRef1 = primaryResponse.departures[sorted[1]].run_ref;
+  runRef2 = primaryResponse.departures[sorted[2]].run_ref;
+  runRef3 = primaryResponse.departures[sorted[3]].run_ref;
+  runRef4 = primaryResponse.departures[sorted[4]].run_ref;
 
-  est0 = new Date(primaryResponse.departures[0].estimated_departure_utc)
-  est1 = new Date(primaryResponse.departures[1].estimated_departure_utc)
-  est2 = new Date(primaryResponse.departures[2].estimated_departure_utc)
-  est3 = new Date(primaryResponse.departures[3].estimated_departure_utc)
-  est4 = new Date(primaryResponse.departures[4].estimated_departure_utc)
+  dest0 = primaryResponse.runs[runRef0].destination_name;
+  dest1 = primaryResponse.runs[runRef1].destination_name;
+  dest2 = primaryResponse.runs[runRef2].destination_name;
+  dest3 = primaryResponse.runs[runRef3].destination_name;
+  dest4 = primaryResponse.runs[runRef4].destination_name;
+// console.log(runRef0);
+  sched0 = new Date(primaryResponse.departures[sorted[0]].scheduled_departure_utc)
+  sched1 = new Date(primaryResponse.departures[sorted[1]].scheduled_departure_utc)
+  sched2 = new Date(primaryResponse.departures[sorted[2]].scheduled_departure_utc)
+  sched3 = new Date(primaryResponse.departures[sorted[3]].scheduled_departure_utc)
+  sched4 = new Date(primaryResponse.departures[sorted[4]].scheduled_departure_utc)
 
-  secondaryRequest0 = "/v3/pattern/run/"+ runRef0 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
-  secondaryRequest1 = "/v3/pattern/run/"+ runRef1 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
-  secondaryRequest2 = "/v3/pattern/run/"+ runRef2 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
-  secondaryRequest3 = "/v3/pattern/run/"+ runRef3 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
-  secondaryRequest4 = "/v3/pattern/run/"+ runRef4 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
+  est0 = new Date(primaryResponse.departures[sorted[0]].estimated_departure_utc)
+  est1 = new Date(primaryResponse.departures[sorted[1]].estimated_departure_utc)
+  est2 = new Date(primaryResponse.departures[sorted[2]].estimated_departure_utc)
+  est3 = new Date(primaryResponse.departures[sorted[3]].estimated_departure_utc)
+  est4 = new Date(primaryResponse.departures[sorted[4]].estimated_departure_utc)
 
-  Promise.all([
-    fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest0 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest0, apiKey).toString()),
-    fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest1 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest1, apiKey).toString()),
-    fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest2 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest2, apiKey).toString()),
-    fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest3 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest3, apiKey).toString()),
-    fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest4 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest4, apiKey).toString())
+  if(primaryResponse.runs[runRef0].express_stop_count >= 1) run0service = "Express"; else run0service = "Stops All";
+  if(primaryResponse.runs[runRef1].express_stop_count >= 1) run1service = "Express"; else run1service = "Stops All";
+  if(primaryResponse.runs[runRef2].express_stop_count >= 1) run2service = "Express"; else run2service = "Stops All";
+  if(primaryResponse.runs[runRef3].express_stop_count >= 1) run3service = "Express"; else run3service = "Stops All";
+  if(primaryResponse.runs[runRef4].express_stop_count >= 1) run4service = "Express"; else run4service = "Stops All";
 
-  ]).then(allResponses => {
-    Promise.all([
-      allResponses[0].json(),
-      allResponses[1].json(),
-      allResponses[2].json(),
-      allResponses[3].json(),
-      allResponses[4].json()
+  if(currentRunRef != runRef0)
+  {
+    console.log("New Service Detected")
+    currentRunRef = runRef0
+  }
 
-    ]).then(data => {
-      // console.log(data[0]);
-      // console.log(data[1]);
-      // console.log(data[2]);
-      // console.log(data[3]);
-      // console.log(data[4]);
-      secondaryResponse0 = data[0];
-      secondaryResponse1 = data[1];
-      secondaryResponse2 = data[2];
-      secondaryResponse3 = data[3];
-      secondaryResponse4 = data[4];
-      // console.log(temp0);
-      // console.log(temp1);
-      // console.log(temp2);
-      // console.log(temp3);
-      // console.log(temp4);
-      // console.log(secondaryResponse0.departures[0].scheduled_departure_utc);
-      // console.log(secondaryResponse1)
-      // console.log(secondaryResponse2)
-      // console.log(secondaryResponse3)
-      // console.log(secondaryResponse4)
-      console.log('Refreshed');
+  else
+  {
+  
+  }
+
+
+  console.log(primaryResponse)
+
+  // secondaryRequest0 = "/v3/pattern/run/"+ runRef0 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
+  // secondaryRequest1 = "/v3/pattern/run/"+ runRef1 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
+  // secondaryRequest2 = "/v3/pattern/run/"+ runRef2 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
+  // secondaryRequest3 = "/v3/pattern/run/"+ runRef3 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
+  // secondaryRequest4 = "/v3/pattern/run/"+ runRef4 + "/route_type/0?expand=all&include_skipped_stops=true&devid=" + devId;
+
+  console.log('Refreshed');
       console.log
       (
         `Mock transport passenger information display\t\t\t\t\t\t\t\t\t\tMaverik Grassby\n` +
         `Built using Public transport victorias API\n` +
         `\n` +
         `${userStationName} Station\t\t\t\t\t\tNext Departures\t\t\t\t\t\tT${clockNow24}\n` +
-        `${date_toTime(sched0)}\t${secondaryResponse0.runs[runRef0].destination_name}\t${date_toUntil(est0, sched0)}\n` +
-        `${date_toTime(sched1)}\t${secondaryResponse1.runs[runRef1].destination_name}\t${date_toUntil(est1, sched1)}\n` +
-        `${date_toTime(sched2)}\t${secondaryResponse2.runs[runRef2].destination_name}\t${date_toUntil(est2, sched2)}\n` +
-        `${date_toTime(sched3)}\t${secondaryResponse3.runs[runRef3].destination_name}\t${date_toUntil(est3, sched3)}\n` +
-        `${date_toTime(sched4)}\t${secondaryResponse4.runs[runRef4].destination_name}\t${date_toUntil(est4, sched4)}\n`
+        `${date_toTime(sched0)}\t${dest0}\t${date_toUntil(est0, sched0)}\t${run0service} Via \n` +
+        `${date_toTime(sched1)}\t${dest1}\t${date_toUntil(est1, sched1)}\t${run0service}\n` +
+        `${date_toTime(sched2)}\t${dest2}\t${date_toUntil(est2, sched2)}\t${run0service}\n` +
+        `${date_toTime(sched3)}\t${dest3}\t${date_toUntil(est3, sched3)}\t${run0service}\n` +
+        `${date_toTime(sched4)}\t${dest4}\t${date_toUntil(est4, sched4)}\t${run0service}\n`
       )
-      // console.log(`Run Reference : ${runRef0}, Destination : ${secondaryResponse0.runs[runRef0].destination_name}, Departure : ${date_toTime(sched0)}, Departing : ${date_toUntil(est0, sched0)}m`);
-      // console.log(`Run Reference : ${runRef1}, Destination : ${secondaryResponse1.runs[runRef1].destination_name}, Departure : ${date_toTime(sched1)}, Departing : ${date_toUntil(est1, sched1)}m`);
-      // console.log(`Run Reference : ${runRef2}, Destination : ${secondaryResponse2.runs[runRef2].destination_name}, Departure : ${date_toTime(sched2)}, Departing : ${date_toUntil(est2, sched2)}m`);
-      // console.log(`Run Reference : ${runRef3}, Destination : ${secondaryResponse3.runs[runRef3].destination_name}, Departure : ${date_toTime(sched3)}, Departing : ${date_toUntil(est3, sched3)}m`);
-      // console.log(`Run Reference : ${runRef4}, Destination : ${secondaryResponse4.runs[runRef4].destination_name}, Departure : ${date_toTime(sched4)}, Departing : ${date_toUntil(est4, sched4)}m`);
-
-
-
-
-      
-    }).catch(error => {
-      console.error(error);
-    });
-  }).catch(error => {
-    console.error(error);
-  });
 
 })();
-
-
 
 }, 1000);
 
@@ -185,3 +174,34 @@ setInterval(() => {
 // fetch("https://timetableapi.ptv.vic.gov.au" + comRequest2 + "&signature=" + CryptoJS.HmacSHA1(comRequest2, apiKey).toString())
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));;
+
+
+
+// Promise.all([
+//   fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest0 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest0, apiKey).toString()),
+//   fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest1 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest1, apiKey).toString()),
+//   fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest2 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest2, apiKey).toString()),
+//   fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest3 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest3, apiKey).toString()),
+//   fetch("http://timetableapi.ptv.vic.gov.au" + secondaryRequest4 + "&signature=" + CryptoJS.HmacSHA1(secondaryRequest4, apiKey).toString())
+
+// ]).then(allResponses => {
+//   Promise.all([
+//     allResponses[0].json(),
+//     allResponses[1].json(),
+//     allResponses[2].json(),
+//     allResponses[3].json(),
+//     allResponses[4].json()
+
+//   ]).then(data => {
+//     // console.log(data[0]);
+//     // console.log(data[1]);
+//     // console.log(data[2]);
+//     // console.log(data[3]);
+//     // console.log(data[4]);
+//      // DO THE THING HERE
+//   }).catch(error => {
+//     console.error(error);
+//   });
+// }).catch(error => {
+//   console.error(error);
+// });
